ce09b573c26e0ece9f2fd498288f437ee27a4f2a
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2024-03-12 14:25:18 -0500

    Se crea el login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { LoginAuthDto } from './dto/login-auth.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,24 +11,8 @@ export class AuthController {
   create(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.create(createAuthDto);
   }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Post('login')
+  loginUser(@Body() loginAuthDto: LoginAuthDto) {
+    return this.authService.login(loginAuthDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,19 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable,UnauthorizedException } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { JwtPayload } from './interfaces/Jwt.payload';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
 import { User } from './entities/auth.entity';
+import { LoginAuthDto } from './dto/login-auth.dto';	
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository:Repository<User>
+    private readonly userRepository:Repository<User>,
+    
+    private readonly jwtService:JwtService
   ){}
   async create(createAuthDto: CreateAuthDto) {
     try{
@@ -28,20 +32,24 @@ export class AuthService {
     }
     
   }
-
-  findAll() {
-    return `This action returns all auth`;
+  private getJwtToken(payload:JwtPayload){
+    
+    const  token=this.jwtService.sign(payload);
+    return token;
   }
+  async login(loginUser:LoginAuthDto){
+    try{
+      const {email,password}=loginUser;
+      const user=await this.userRepository.findOneBy({email});
+      if(!user) throw new UnauthorizedException('User or password incorrect');
+      const isValid=bcrypt.compareSync(password, user.password);
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+      if(!isValid) throw new UnauthorizedException('User or password incorrect');
+      const jwt=this.getJwtToken({email});
+      return {email,jwt};
+    }
+    catch(e){
+      throw new UnauthorizedException('User or password incorrect');
+    }
   }
 }
